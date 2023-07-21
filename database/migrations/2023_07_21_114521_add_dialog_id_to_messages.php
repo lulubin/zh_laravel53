4de843e7494dda09bc3458620e05f4b9b648a350
lulubin <452936616@qq.com>
2023-07-21 13:58:25 +0800

    查看私信

--- database/migrations/2023_07_21_114521_add_dialog_id_to_messages.php
+++ database/migrations/2023_07_21_114521_add_dialog_id_to_messages.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDialogIdToMessage extends Migration
+class AddDialogIdToMessages extends Migration
 {
     /**
      * Run the migrations.
